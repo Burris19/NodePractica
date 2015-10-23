57337334d3ac90565b91f6541ad88b6326a5bad0
Burris19 <julianhernandez19@gmail.com>
2015-10-23 00:41:45 -0600

    [add] api Rest with Sail Framework :heart_eyes:

---
+++ curso/cursoNode/Sails/apiPosts/api/models/Post.js
@@ -0,0 +1,26 @@
+/**
+* Post.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+        title: {
+            type: 'String',
+            unique: true
+        },
+        content: {
+            type: 'String',
+            unique: true
+        },
+        author: {
+            type: 'String'
+        }
+
+
+  }
+};
+
